b563738fab2a2e91c6808ae5a95ece43ed907eb4
xiao <927264891@qq.com>
2018-08-14 16:20:17 +0800

    第一次提交git

---
+++ src/com/xiaoguai/xiaoqiang/hello.java
@@ -0,0 +1,11 @@
+package com.xiaoguai.xiaoqiang;
+/**
+* @author 作者 :  XiaoQiang
+* @version 创建时间：2018年8月14日 下午4:12:59
+* 类说明
+*/
+public class hello {
+	public static void main(String[] args) {
+		System.out.println("你好");
+	}
+}
